ff2b8ffa941e26418d4674c50b22a20c724974e3
chris cho <wheogus98@gmail.com>
2025-05-25 18:28:52 -0700

    changed base url

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/jenna_teacher/',     // ← your repo name, leading and trailing slashes
+  base: '/',     // ← your repo name, leading and trailing slashes
   plugins: [react()],
 });
